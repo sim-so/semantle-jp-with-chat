8278129bb65318f7d975a29a66c58989d38c5b07
Sohyun Sim <sim-so@users.noreply.huggingface.co>
2023-07-25 03:00:09 +0000

    ver1.2 (#2)
    - app.py revision (8617bcbdf342100258bb8bc61196d863ec326fc5)
    - edit app.py (e483d1b274baf247dd94a18ffed1a16d0b9a9951)
    - add rule.md (8a877588b255cc249ab7c14e1bfab1fc98533f69)
    - update prompt (e05b307d4753eb1353cc880efaa95e3e643e6c92)
    - edit title (f5dafc759489a74e37adae4989bd6a626ef0c095)
    - delete cache (f90ef2e8b719581ef4820b9c8e8191fddd57c408)
    - add .gitignore (2378d2f3acb7ea7b8ae204077e6425ae14d15432)

--- src/functions.py
+++ src/functions.py
@@ -7,42 +7,29 @@ guess_word = {"name": "guess_word",
                               "type": "string",
                               "description": "A single Japanese word to guess, which is can be a noun, verb, adverb or adjective. e.g. 空, 近い, 行く, etc."
                               },
-                          "puzzle_num": {
-                              "type": "integer",
-                              "description": "An index indicating today's puzzle."
-                          }
+                        #   "puzzle_num": {
+                        #       "type": "integer",
+                        #       "description": "An index indicating today's puzzle."
+                        #   }
                       },
-                      "required": ["word", "puzzle_num"]
+                      "required": ["word"]
                   }}
 
-prepare_hint = {"name": "prepare_hint",
-                "description": "Use this function to retrieve information for hint.",
+lookup_answer = {"name": "lookup_answer",
+                "description": "Use this function to check the correct answer of today's puzzle.",
                 "parameters": {
                     "type": "object",
                     "properties": {
-                        "puzzle_num": {
-                            "type": "interger",
-                            "description": "An index for today's puzzle set."
+                        # "puzzle_num": {
+                        #     "type": "integer",
+                        #     "description": "An index indicating today's puzzle."
                         }
-                    },
-                    "required": ["puzzle_num"]
+                    # },
+                    # "required": ["puzzle_num"]
                 }}
 
-get_secret = {"name": "get_secret",
-                "description": "You can check what the correct answer of today's puzzle is by this function.",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "puzzle_num": {
-                            "type": "integer",
-                            "description": "An index indicating today's puzzle."
-                        }
-                    },
-                    "required": ["puzzle_num"]
-                }}
-
-get_puzzle_num = {"name": "get_puzzle_num",
-                  "description": "Use this function to check today's puzzle number.",
+retrieve_puzzle_num = {"name": "retrieve_puzzle_num",
+                  "description": "Use this function to retrieve today's puzzle number.",
                   "parameters": {
                       "type": "object",
                       "properties": {}
@@ -60,13 +47,23 @@ update_history = {"name": "update_history",
                           },
                           "guess_history": {
                               "type": "object",
-                              "description": "A dataframe containing the guessed words and its xore and rank in a row."
+                              "description": "A dataframe containing the guessed words and its score and rank in a row."
                           }
                       },
                       "required": ["current_guess", "guess_history"]
                   }}
+read_rule = {"name": "read_rule",
+                 "description": "Use this function to read the game rule for clarification of your response.",
+                 "parameters": {
+                     "type": "object",
+                     "properties": {},
+                 }}
 
 
 def get_functions():
-    functions = [guess_word]
+    functions = [guess_word, 
+                 lookup_answer, 
+                #  retrieve_puzzle_num, 
+                #  update_history, 
+                 read_rule]
     return functions
